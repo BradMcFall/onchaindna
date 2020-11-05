2689b98ed47e4dcf41dba632e987d0262cf9012f
bradfall <bradfall@DESKTOP-6245RT2.localdomain>
2020-11-05 10:01:29 -0500

    Committer: bradfall <bradfall@DESKTOP-6245RT2.localdomain>  Your branch is up to date with 'origin/main'. 	new file:   app/src/components/UserForm.vue 	modified:   app/src/views/Sell.vue

---
+++ app/src/components/UserForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="userform">
+    <h1>{{msg}}</h1>
+     <p> Your wallet address contains 1 AVAX and you can view it's contents at
+     </p>
+     <form @submit="checkForm">
+      <ul>
+        <li>
+      <input type="text" v-model="username" required="required"
+             placeholder="username" />
+        </li>
+        <li>
+      <input type="text" v-model="password" required="required"
+             placeholder="password" />
+        </li>
+        <li>
+      <button type="submit">Create User</button>
+        </li>
+      </ul>
+    </form>
+
+  </div>
+</template>
--- app/src/views/Sell.vue
+++ app/src/views/Sell.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="sell">
     <h1>You have three mutations you may want to sell...</h1>
+    <UserForm msg="Enter a username and password and click to obtain a funded MDNANFT wallet."/>
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import UserForm from '@/components/UserForm.vue'
+
+@Options({
+  components: {
+    UserForm
+  }
+})
+
+export default class Sell extends Vue {}
+</script>
